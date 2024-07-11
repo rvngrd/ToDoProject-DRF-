28e2912f3b97d92a37f466d61cbfb5b889a3b7be
rvngrd <rrvngrdd@gmail.com>
2024-07-11 19:57:04 +0330

    End of part 3

--- todo/models.py
+++ todo/models.py
@@ -9,5 +9,9 @@ class Todo(models.Model):
     priority = models.IntegerField(default=1)
     is_done = models.BooleanField(default=False)
 
+
+    def __str__(self) -> str:
+        return f'{self.title} / Is Done: {self.is_done}'
+
     class Meta:
         db_table = 'todos'
