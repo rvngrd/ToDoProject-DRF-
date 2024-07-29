b0ed3b5c6a687d74695041150534545dfde970a8
rvngrd <rrvngrdd@gmail.com>
2024-07-29 09:44:58 +0330

    End of part 14

--- config/settings.py
+++ config/settings.py
@@ -129,5 +129,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2 
+    'PAGE_SIZE': 2,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication' 
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated' 
+    ]
 }
